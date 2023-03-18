b65edea9a302c000a63c849fe8e738f23bb85e55
Jeff Rabovsky <jeff.rabovsky@basis.net>
2023-03-18 18:49:23 -0500

    falling ice blocks work

--- README.md
+++ README.md
@@ -3,5 +3,5 @@ Penguin Peril
 
 Move Waddles the penguin with the arrow keys to avoid the falling ice!
 
-Built with https://p5js.org/
-See https://fal-works.github.io/p5js-templates/use-ts-in-js.html for more details on type checking
+* Built with https://p5js.org/
+* See https://fal-works.github.io/p5js-templates/use-ts-in-js.html for more details on type checking
--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 let img;
 let gameState;
+const iceWidth = 50;
+const iceHeight = 100;
+
+function rectCollision(x1, y1, w1, h1, x2, y2, w2, h2){
+    return x1 < x2 + w2 &&
+           x1 + w1 > x2 &&
+           y1 < y2 + h2 &&
+           h1 + y1 > y2;
+}
 
 function preload() {
     img = loadImage('waddles.png');
@@ -9,19 +18,64 @@ function setup() {
     createCanvas(800, 600);
     gameState = {
         playerPosition: {
-            x: width / 2 - img.width / 2,
-            y: height - img.height
-        }
+            x: width / 2,
+            y: height - img.height / 2
+        },
+        ices: [],
+        framesPerNewIce: 12,
+        score: 0,
+        gameOver: false
     }
 }
 
 function draw() {
-    if (keyIsDown(LEFT_ARROW)) {
-        gameState.playerPosition.x -= 1;
-    } else if (keyIsDown(RIGHT_ARROW)) {
-        gameState.playerPosition.x += 1;
+    if (!gameState.gameOver) {
+        if (keyIsDown(LEFT_ARROW) && gameState.playerPosition.x > img.width / 2) {
+            gameState.playerPosition.x -= 1;
+        } else if (keyIsDown(RIGHT_ARROW) && gameState.playerPosition.x + img.width / 2 < width) {
+            gameState.playerPosition.x += 1;
+        }
+
+        if (frameCount % gameState.framesPerNewIce == 0){
+            gameState.ices.push({
+                x: random(0, width),
+                y: -iceHeight / 2,
+                speed: 0
+            })
+            gameState.score += 1;
+        }
+
+        gameState.ices.forEach(function(ice){
+            ice.speed += 0.1;
+            ice.y += ice.speed;
+        });
+
+        gameState.ices = gameState.ices.filter(function(ice){
+            return ice.y + iceHeight / 2 < height;
+        });
+
+        gameState.ices.forEach(function(ice){
+            let collision = rectCollision(
+                ice.x, ice.y, iceWidth, iceHeight,
+                gameState.playerPosition.x, gameState.playerPosition.y, img.width, img.height
+            );
+            if (collision){
+                gameState.gameOver = true;
+            }
+        });
     }
+
+    imageMode(CENTER);
+    rectMode(CENTER);
+    textAlign(CENTER, CENTER);
+    textSize(32);
 
     background(220);
     image(img, gameState.playerPosition.x, gameState.playerPosition.y);
+    gameState.ices.forEach(function(ice){
+        rect(ice.x, ice.y, iceWidth, iceHeight);
+    });
+    if (gameState.gameOver) {
+        text(`Game over! Score: ${gameState.score}`, width / 2, height / 2);
+    }
 }
